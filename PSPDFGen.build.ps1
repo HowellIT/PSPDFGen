bf8bb5b1675d1cd351dfb27dafb8e98933082968
Anthony Howell <anthony@howell-it.com>
2019-03-01 10:46:54 -0800

    incremented version to publish

--- PSPDFGen.build.ps1
+++ PSPDFGen.build.ps1
@@ -4,7 +4,7 @@ $docPath = "$PSScriptRoot\docs"
 $moduleName = "PSPDFGen"
 $modulePath = "$buildPath\$moduleName"
 $author = 'Anthony Howell'
-$version = '0.0.1'
+$version = '0.0.2'
 
 task Clean {
     If(Get-Module $moduleName){
